e4a9eb586f622006bd0348771b4152e24e7b6b7a
VIKAS <bellagantivikasreddy@gmail.com>
2026-01-21 17:39:54 -0600

    day 1 basics

---
+++ src/main/java/com/practice/accepttwonumbers.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class accepttwonumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter the second number1:");
+        double num2 = sc.nextDouble();
+
+        double sum = num1+num2;
+        double average = sum/ 2;
+        System.out.println("sum =" + sum);
+        System.out.println("average"+ average);
+        sc.close();
+    }
+
+
+
+}
