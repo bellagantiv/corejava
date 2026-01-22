9d727b58a5d24c4ff5d0d55edff6df2cf9a556b6
VIKAS <bellagantivikasreddy@gmail.com>
2026-01-21 18:44:17 -0600

    added one more

---
+++ src/main/java/com/practice/cubeofanumber.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class cubeofanumber {
+    public static void main(String[] args){
+        Scanner sc =   new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+        int cube = num * num * num;
+        System.out.println("cube of the number " + cube );
+    }
+}
