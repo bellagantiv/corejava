8e83b16c76fdff0f3470cc22c30e4522d0594b43
VIKAS <bellagantivikasreddy@gmail.com>
2026-01-21 20:01:38 -0600

    5 th

---
+++ src/main/java/com/practice/netandaverage.java
@@ -0,0 +1,21 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class netandaverage {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double sum = 0;
+        double weight, average;
+        for(int i=1; i <= 5; i++) {
+            System.out.println("enter weight of person" + i + ":");
+            weight = sc.nextDouble();
+            sum = sum + weight;
+        }
+    average = sum / 5;
+     System.out.println("net weight =" +  sum);
+     System.out.println("aveerage weight =" + average);
+
+
+    }
+}
